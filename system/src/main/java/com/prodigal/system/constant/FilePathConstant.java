d9dfaec4a333bcf966f0d1ad9e04521ca4bee64e
lang <3510483474@qq.com>
2024-12-13 11:41:15 +0800

    prodigal-picture author:Lang version:1.0.5 content: 【公共模块】-后端 1、新增图片上传路径

---
+++ system/src/main/java/com/prodigal/system/constant/FilePathConstant.java
@@ -0,0 +1,21 @@
+package com.prodigal.system.constant;
+
+/**
+ * @program: prodigal-picture
+ * @author: Lang
+ * @description: 路径常量
+ **/
+public interface FilePathConstant {
+    /**
+     * 总目录
+     */
+    String uploadPath = "picture";
+    /**
+     * 私有目录
+     */
+    String PICTURE_PRIVATE_PREFIX = "picture/private";
+    /**
+     * 公共目录
+     */
+    String PICTURE_PUBLIC_PREFIX = "picture/public";
+}
